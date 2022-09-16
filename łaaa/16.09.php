d546e8689e4d0edf33212b772282f9f8eb07bbff
Olek-Zawadzki <74409092+Olek-Zawadzki@users.noreply.github.com>
2022-09-16 16:24:52 +0200

    g

--- łaaa/16.09.php
+++ łaaa/16.09.php
@@ -10,9 +10,19 @@
     <form method="GET" action="16.09.php">
         <fieldset>
             <legend>działania na prostopadłościanie</legend>
+            <input type="number" name="num1">
+            <input type="number" name="num2">
+            <input type="number" name="num3"><br>
+            <input type="submit" value="oblicz"><br>
+            <select name="dzialania">
+            <option value="v">objętość</option>
+            <option value="p">pole</option>
+            <option value="d">przekątna</option>
+            </select>
         </fieldset>
     </form>
     <?php
+    
     ?>
 </body>
 </html>
